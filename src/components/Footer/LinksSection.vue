8b19f1991c2595ce25dca58a0483a6f493141560
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-11-09 17:46:44 +0100

    added static link section (footer)

--- src/components/Footer/LinksSection.vue
+++ src/components/Footer/LinksSection.vue
@@ -1,5 +1,62 @@
 <template>
-    
+    <section class="links-section">
+        <div class="container d-flex py-3 d-flex justify-content-between">
+            <div class="container d-flex d-flex">
+                <div class="d-flex flex-column">
+                    <ul>
+                        <li>
+                            <h1>lorem</h1>
+                        </li>
+                        <li>Text</li>
+                        <li>Text</li>
+                        <li>Text</li>
+                        <li>Text</li>
+                        <li>Text</li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <h1>lorem</h1>
+                        </li>
+                        <li>Text</li>
+                        <li>Text</li>
+                    </ul>
+                </div>
+                <ul class="mx-3">
+                    <li>
+                            <h1>lorem</h1>
+                        </li>
+                    <li>Text</li>
+                    <li>Text</li>
+                    <li>Text</li>
+                    <li>Text</li>
+                    <li>Text</li>
+                    <li>Text</li>
+                    <li>Text</li>
+                </ul>
+                <ul>
+                    <li>
+                            <h1>lorem</h1>
+                        </li>
+                    <li>Text</li>
+                    <li>Text</li>
+                    <li>Text</li>
+                    <li>Text</li>
+                    <li>Text</li>
+                </ul>
+            </div>
+            <figure><img src="/public/image/dc-logo-bg.png" alt=""></figure>
+            <!-- CORREGGERE IMMAGINE -->
+            <!-- CORREGGERE IMMAGINE -->
+            <!-- CORREGGERE IMMAGINE -->
+            <!-- CORREGGERE IMMAGINE -->
+            <!-- CORREGGERE IMMAGINE -->
+            <!-- CORREGGERE IMMAGINE -->
+            <!-- CORREGGERE IMMAGINE -->
+            <!-- CORREGGERE IMMAGINE -->
+            <!-- CORREGGERE IMMAGINE -->
+            <!-- CORREGGERE IMMAGINE -->
+        </div>
+    </section>
 </template>
 
 <script>
